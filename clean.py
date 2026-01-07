66c8700b3cd96b22405c048661e9a83335214c81
jet-inigo <jetgeronimo@gmail.com>
2026-01-07 00:28:11 -0600

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import argparse
 import os
+import re
+
+FILE_NAME_REGEX: re.Pattern[str] = re.compile(r"[\w/._-]+\.(?:png|jpe?g|heic)", re.IGNORECASE)
 
 parser = argparse.ArgumentParser("py " + os.path.basename(__file__))
 _ = parser.add_argument("file")
@@ -9,8 +12,8 @@ args = parser.parse_args()
 
 df = pd.read_csv(args.file)
 
-# effects = df["content"].str.extractall(r"Sent with (?P<effect>[\w ]+)")
-# print(effects.value_counts())
+effects = df["content"].str.extractall(r"Sent with (?P<effect>[\w ]+)")
+print(effects.value_counts())
 
 tapbacks = df["content"].str.extractall(r"Tapbacks:\n(?P<tapback>\w+) by (?P<name>[\w ]+)")
 print(tapbacks.value_counts())
@@ -18,9 +21,14 @@ print(tapbacks.value_counts())
 cleaned_df = df.copy(deep=True)
 cleaned_df = cleaned_df[~cleaned_df["content"].str.contains("GamePigeon message", regex=False, na=False)]
 cleaned_df = cleaned_df[~cleaned_df["content"].str.contains("Apple Cash transaction", regex=False, na=False)]
+cleaned_df = cleaned_df[~cleaned_df["content"].str.contains("Messages Image", regex=False, na=False)]
 cleaned_df = cleaned_df[~cleaned_df["content"].str.contains(r"Sticker from (?:[\w ]+)", regex=True, na=False)]
 cleaned_df = cleaned_df[~cleaned_df["content"].str.contains("This message responded to an earlier message.", na=False)]
-cleaned_df["content"] = cleaned_df["content"].str.replace(r"Tapbacks:\n(?:\w+) by (?:[\w ]+)", "", regex=True)
+cleaned_df["content"] = cleaned_df["content"].str.replace("’", "'", regex=False)
+cleaned_df["content"] = cleaned_df["content"].str.replace(r"Tapbacks:\n(?:\w+) by (?:[^\n]+)", "", regex=True)
+cleaned_df["content"] = cleaned_df["content"].str.replace(r"Sent with (?P<effect>[^\n]+)", "", regex=True)
+cleaned_df["content"] = cleaned_df["content"].str.replace(FILE_NAME_REGEX, "", regex=True)
 cleaned_df["content"] = cleaned_df["content"].str.strip()
 
+print(cleaned_df.shape)
 cleaned_df.to_csv(args.output, index=False)
